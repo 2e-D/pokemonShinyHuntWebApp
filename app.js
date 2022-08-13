9c0c0ebf98578e6372281b395ebc2782d2565029
Dan Toohey <100450914+2e-D@users.noreply.github.com>
2022-08-13 21:45:57 +0100

    Changed the method of API fetching
    used a function instead to fetch API instead

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const pokeApi = fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+function getPokemon() {
+fetch('https://pokeapi.co/api/v2/pokemon/ditto')
     .then((response) => response.json())
     .then((data) => console.log(data));
-
-
-
-let countUp = document.getElementById("counter")
-
-console.log(countUp)
-let count = 0
-
-
-function increment() {
-    count = count + 1
-    countUp.innerText = count
+    
 }
 
+ /* COUNTER */
+let countUp = document.getElementById("counter") // Assigns counter in HTML to a variable and grabs it
 
+console.log(countUp)
+let count = 0        // sets a count variable
+
+
+function increment() { // links to the increment function i set in HTML and calls it everytime the button is clicked
+    count = count + 1
+    countUp.innerText = count // Changes the Text from '0' to the count variable
+}
+
+// Unsure wether to add decrement button
 function decrement() {
     count = count - 1
     countUp.innerText = count
 }
 
+/* COUNTER END */
+  // PAGE LOADER
 
+let loader = document.getElementById("preloader");
 
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
