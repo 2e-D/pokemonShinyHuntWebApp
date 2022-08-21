232c1aba68cc34d86dbb308d2a0b19283155dd30
Dan Toohey <100450914+2e-D@users.noreply.github.com>
2022-08-21 12:55:34 +0100

    Added Greeting depending on time of day

--- landingPage.js
+++ landingPage.js
@@ -9,3 +9,12 @@ if (7 <= currentTime && currentTime < 18) {
 } else {if (document.body) {
     document.body.className = "landingImageNight"
 }}
+
+
+
+let currentTime2 = new Date().getHours();
+if ( 4 <= currentTime2 && currentTime2 < 12){
+    document.getElementById('Greeting').innerHTML = 'Good Morning'
+} else if (12 <= currentTime2 && currentTime2 < 17){
+    document.getElementById('Greeting').innerHTML = 'Good Afternoon'
+} else if (17 <= currentTime2 && currentTime2 < 4){ document.getElementById('Greeting').innerHTML = 'Good Evening'}
